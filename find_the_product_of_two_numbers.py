fcfc8941a4854a4f91e05a0fdaa5597f5cfaadfc
Jossel Redoquerio <josselgredoquerio@iskolarngbayan.pup.edu.ph>
2024-01-21 01:15:12 +0800

    step

--- find_the_product_of_two_numbers.py
+++ find_the_product_of_two_numbers.py
@@ -7,3 +7,12 @@
 # Expected Output: The result is 600
 # Given 2: number1 = 40 , number2 = 30
 # Expected Output: The result is 70
+
+
+# calculate product of two number
+# check if product is less then 1000
+# product is greater than 1000 calculate sum
+     
+    
+# first condition
+# Second condition
